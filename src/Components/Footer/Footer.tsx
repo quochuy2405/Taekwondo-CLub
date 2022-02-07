2d65fbdae2a0dcf1e35a84428425758997d3ea96
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-02-07 14:23:32 +0700

    done

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Logo from '../../Image/logo.png'
+import gmail from '../../Icon/gmail.png'
+import facebook from '../../Icon/facebook.png'
+import fanpage from '../../Icon/friends.png'
+import zalo from '../../Icon/zalo.png'
 import './Footer.scss'
 function Footer() {
   return (
@@ -8,26 +12,39 @@ function Footer() {
         <img src={Logo} alt="" />
       </div>
       <div className="contain-link">
-        <ul className="list-none">
+        <ul className="list-none list-social">
           <li>
-            <a href="">Facebook</a>
+            <a href="https://www.facebook.com/nguyenhuutuantkd">
+              <span>
+                <img src={facebook} alt="Facebook" className="icon-footer" />
+              </span>
+              <p className="text-icon">Facebook</p>
+            </a>
           </li>
           <li>
             {' '}
-            <i className="far fa-plus-square"></i>
-            <a href="">Email</a>
+            <a href="https://mail.google.com/mail/u/1/#inbox?compose=GTvVlcRwPxWkwfDMrbqSRKHWwQHMRlgmgxHvqwBlmpTkRhDBJDZQJXLwqQwzMmmdDDdnFQWWmpjGh">
+              <span>
+                <img src={gmail} alt="Email" className="icon-footer" />
+              </span>
+              <p className="text-icon">Email</p>
+            </a>
           </li>
           <li>
-            <a href="">Fanpage</a>
+            <a href="https://www.facebook.com/Taekwondo-Kr%C3%B4ng-Pa-alpha-105670594988534">
+              <span>
+                <img src={fanpage} alt="Fanpage" className="icon-footer" />
+              </span>
+              <p className="text-icon">Fanpage</p>
+            </a>
           </li>
           <li>
-            <a href="">Zalo</a>
-          </li>
-          <li>
-            <a href="">Trụ sở chính</a>
-          </li>
-          <li>
-            <a href="">Liên hệ</a>
+            <a href="#">
+              <span>
+                <img src={zalo} alt="Zalo" className="icon-footer" />
+              </span>
+              <p className="text-icon">Zalo</p>
+            </a>
           </li>
         </ul>
       </div>
